2b061de1eb9f74c8aedb21535c0eb5fde139ceab
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-05-17 12:23:13 -0600

    Added transitions from mm to wm

--- app/src/main/java/com/example/chon/MainActivity.java
+++ app/src/main/java/com/example/chon/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.chon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configWheelMenuButton();
     }
 
-    /**
-     * Called when user selected "SELECT WHEEL"
-     */
-    public void goToWheelMenu(View view) {
-        setContentView(R.layout.activity_wheel_menu);
+    private void configWheelMenuButton() {
+        Button wheelMenuButton = (Button) findViewById(R.id.wheelMenuButton);
+        wheelMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, WheelMenu.class));
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/chon/WheelMenu.java
+++ app/src/main/java/com/example/chon/WheelMenu.java
@@ -2,7 +2,10 @@ package com.example.chon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WheelMenu extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class WheelMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wheel_menu);
+
+        configMainMenuButton();
     }
+
+    private void configMainMenuButton() {
+        Button wheelMenuButton = (Button) findViewById(R.id.mainMenuButton);
+        wheelMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WheelMenu.this, MainActivity.class));
+            }
+        });
+    }
+
 }
