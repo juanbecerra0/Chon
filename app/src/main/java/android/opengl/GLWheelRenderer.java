adfbe7c1305b1b03d8e950725e8b14c1fd07a3c2
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-05-27 23:20:02 -0600

    Added rotation animation to square

--- app/src/main/java/android/opengl/GLWheelRenderer.java
+++ app/src/main/java/android/opengl/GLWheelRenderer.java
@@ -1,5 +1,7 @@
 package android.opengl;
 
+import android.os.SystemClock;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -12,6 +14,9 @@ public class GLWheelRenderer implements GLSurfaceView.Renderer {
     private final float[] vPMatrix = new float[16];
     private final float[] projectionMatrix = new float[16];
     private final float[] viewMatrix = new float[16];
+
+    // Used in rotation animation
+    private float[] rotationMatrix = new float[16];
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -44,6 +49,20 @@ public class GLWheelRenderer implements GLSurfaceView.Renderer {
         // Calculate the projection and view transformation
         Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
 
-        mSquare.draw(vPMatrix);
+        // Rotation animation
+        float[] scratch = new float[16];
+
+        // Create a rotation transformation for the triangle
+        long time = SystemClock.uptimeMillis() % 4000L;
+        float angle = 0.090f * ((int) time);
+        Matrix.setRotateM(rotationMatrix, 0, angle, 0, 0, -1.0f);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the vPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0);
+
+        //mSquare.draw(vPMatrix);
+        mSquare.draw(scratch);
     }
 }
